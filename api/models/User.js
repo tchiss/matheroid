6bd4bab65e8c8deaaae0a02fc2c73b51ebb3093f
Nestor Alain <tchiss_a@epitech.eu>
2015-01-16 10:51:03 +0100

    Matheroid project

---
+++ api/models/User.js
@@ -0,0 +1,39 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  //schema: true,
+  attributes: {
+    name: { type: 'string', required: true },
+    username: { type: 'string', required: true },
+    email: { type: 'string', lowercase: true, email: true, required: true, unique: true },
+    role: { type: 'string', default: 'user' },
+    hashedPassword: { type: 'string'},
+    provider: 'string',
+    salt: 'string',
+    facebook: {},
+    twitter: {},
+    google: {},
+    github: {},
+
+    toJSON: function(){ var obj = this.toObject(); delete obj.password; delete obj.hashedPassword; delete obj.confirmation; delete obj._csrf; return obj;}
+  },
+
+  beforCreate: function(values, next){
+    if(!values.password || values.password != values.confirmation) {
+      return next({err: ['Password doesn\' match password confirmation']});
+    }
+
+    required('bcrypt').hash(values.password, 10, function passwordHashed(err, hashedPassword) {
+      if (err) return next(err);
+      values.hashedPassword = hashedPassword;
+      //values.online = true;
+      next();
+    });
+  }
+};
+
