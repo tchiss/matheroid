6e3a041d63459833afcdd7310fb79d152b4f2ad2
Nestor Alain TCHISSAMBOT MAKOSSO <tchiss_a@MacBook-Pro-de-Nestor.local>
2015-01-31 23:36:18 +0100

    Join & Leave for Room Controller

--- api/controllers/RoomController.js
+++ api/controllers/RoomController.js
@@ -5,7 +5,25 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+ var _ = require('underscore');
+ var Room = require('../models/Room');
+
 module.exports = {
-	
+	'join': function(req, res, next){ // Joindre la room
+		var roomId = req.param('roomId');
+		Room.findOne({id: roomId}).populate('users').exec(function(err, room){
+			var currentUserIsInRoom = _.find(room.users, function(user){ return user.id==req.user.id; });
+			if (currentUserIsInRoom == null)
+				Room.subscribe(req, roomId, ['message']);
+		});
+		return next();
+	},
+
+	'leave': function(req, res, next){ // Quitter la room
+		var roomId = req.param('roomId');
+		Room.unsubscribe(req, roomId, ['message']);
+
+		return next();
+	}
 };
 
