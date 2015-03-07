d1ad2046ce1cc5cdf106176f7e64d7f6f2603dc5
tchiss <alainwilfrid@yahoo.com>
2015-03-07 20:16:38 +0100

    Sotries Begining

--- api/controllers/StoriesController.js
+++ api/controllers/StoriesController.js
@@ -7,10 +7,22 @@
 
 module.exports = {
 	'index': function(req, res, next){
-
+		// index page 
 	},
 
 	'read': function(req, res, next){
+		
+	},
+
+	'create': function(req, res, next){
+		
+	},
+
+	'update': function(req, res, next){
+		
+	},
+
+	'destroy': function(req, res, next){
 		
 	}
 };
